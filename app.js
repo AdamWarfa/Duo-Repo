0b0db9ef28b87d1f595e3706d35244dca4a5bad1
AdamWarfa <Adamwarfadk@gmail.com>
2023-04-27 09:52:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,4 @@
 
 window.addEventListener("load", initApp);
 
-console.log("HELLO WORLD");
-
-function initApp() {
-  console.log("shoutout my niggas from the block");
-  console.log(" ARGHHHH ,I ALMOST DROPPED MY CROISSANT !");
-}
-
-function onTheBlock() {
-  console.log("hej");
-}
-
-function onTheBlock() {
-  console.log("WASUUUUUUUP");
-}
+function initApp() {}
